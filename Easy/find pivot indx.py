cbb390109b386d9318cb1a240dc4333a467ae0b2
DiwanDev <dev.diwan1308@gmail.com>
2026-08-20 22:31:54 +0530

    design hashset

---
+++ Easy/find pivot indx.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        n = len(nums)
+        prefixSum = [0] * (n + 1)
+        for i in range(n):
+            prefixSum[i + 1] = prefixSum[i] + nums[i]
+
+        for i in range(n):
+            leftSum = prefixSum[i]
+            rightSum = prefixSum[n] - prefixSum[i + 1]
+            if leftSum == rightSum:
+                return i
+        return -1
+        
---
+++ Easy/height checker.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def heightChecker(self, heights):
+        expected = sorted(heights)
+        res = 0
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                res += 1
+        return res
+        
