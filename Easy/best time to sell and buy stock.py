8c8d98cf73c4d77318271e11c5c987c9d7d64739
DiwanDev <dev.diwan1308@gmail.com>
2026-09-12 23:14:35 +0530

    change in code

--- Easy/best time to sell and buy stock.py
+++ Easy/best time to sell and buy stock.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
+        r = 0
         for i in range(len(prices)):
             buy = prices[i]
             for j in range(i + 1, len(prices)):
                 sell  = prices[j]
-                res = max(res, sell - buy)
-        return res
+                r = max(r, sell - buy)
+        return r
