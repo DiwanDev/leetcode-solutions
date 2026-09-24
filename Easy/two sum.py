090a051e8df49110ea105e7a1e28ef7ae345e0cb
DiwanDev <dev.diwan1308@gmail.com>
2026-09-24 23:16:14 +0530

    change code

--- Easy/two sum.py
+++ Easy/two sum.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        for k in range(len(nums)):
+            for j in range(k + 1, len(nums)):
+                if nums[k] + nums[j] == target:
+                    return [k, j]
         return []
     
